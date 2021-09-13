0a879fe54268392cc30591301111e9d3615b14be
siddharthkp <siddharth.kshetrapal@gmail.com>
2021-09-13 17:23:16 +0200

    wrap script in a document undefined check for serverside

--- packages/babel-plugin-open-source/script.js
+++ packages/babel-plugin-open-source/script.js
@@ -1,7 +1,9 @@
-document.addEventListener('click', (event) => {
-  if (!event.target.dataset.source) return;
-  if (!event.altKey) return;
+if (typeof document !== 'undefined') {
+  document.addEventListener('click', (event) => {
+    if (!event.target.dataset.source) return;
+    if (!event.altKey) return;
 
-  const { filename, start } = JSON.parse(event.target.dataset.source)
-  window.open('vscode://file' + filename + ':' + start)
-})
+    const { filename, start } = JSON.parse(event.target.dataset.source)
+    window.open('vscode://file' + filename + ':' + start)
+  })
+}
