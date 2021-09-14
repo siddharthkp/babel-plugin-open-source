856633dadb726aad4a62b0923053591d34c34f85
Giuseppe <giuseppeg@users.noreply.github.com>
2021-09-14 14:34:52 +0200

    Add feature to generate the file url
    This should allow to use the plugin with sublime too (my editor of choice)

--- packages/babel-plugin-open-source/script.js
+++ packages/babel-plugin-open-source/script.js
@@ -3,7 +3,8 @@ if (typeof document !== 'undefined') {
     if (!event.target.dataset.source) return;
     if (!event.altKey) return;
 
-    const { filename, start } = JSON.parse(event.target.dataset.source)
-    window.open('vscode://file' + filename + ':' + start)
+    const { filename, start } = JSON.parse(event.target.dataset.source);
+    const createFileUrl = window.createBabelPluginOpenSourceFileUrl || ({ filename, start }) => `vscode://file${filename}:${start}`;
+    window.open(createFileUrl({ filename, start }));
   })
 }
