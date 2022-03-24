5f00befe38b77e3aad4d6e7ae77516bb5d11513e
Siddharth Kshetrapal <siddharthkp@github.com>
2022-03-24 18:25:08 +0100

    move logic into its own file

--- packages/babel-plugin-open-source/babel.js
+++ packages/babel-plugin-open-source/babel.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const ini = require('ini');
 const { declare } = require('@babel/helper-plugin-utils');
 const { types: t } = require('@babel/core');
 const dotenv = require('dotenv');
@@ -77,6 +75,7 @@ module.exports = declare((api) => {
       } else if (editor === 'vscode-insiders') {
         url = `vscode-insiders://file/${state.filename}:${location.start.line}`;
       } else if (editor === 'github') {
+        const getGitHubUrl = require('./github-url');
         url = getGitHubUrl(state.filename, location.start.line);
       } else {
         url = `vscode://file/${state.filename}:${location.start.line}`;
@@ -94,39 +93,3 @@ module.exports = declare((api) => {
     visitor
   };
 });
-
-const getGitHubUrl = (localFilePath, lineNumber) => {
-  if (process.env.VERCEL) {
-    // TODO: I bet I'll regret hardcoding this
-    const gitRoot = '/vercel/path0/';
-
-    const repo = process.env.VERCEL_GIT_REPO_OWNER + '/' + process.env.VERCEL_GIT_REPO_SLUG;
-    const filePath = localFilePath.replace(gitRoot, '');
-
-    // TODO: replace with ci-env
-    const branchName = process.env.VERCEL_GIT_COMMIT_REF || 'main';
-
-    return `https://github.com/${repo}/blob/${branchName}/${filePath}#L${lineNumber}`;
-  } else {
-    try {
-      const findGitRoot = require('find-git-root');
-      const gitRoot = findGitRoot(localFilePath).replace('.git', '');
-      const repo = getRepository(gitRoot);
-      const filePath = localFilePath.replace(gitRoot, '');
-
-      // TODO: replace with ci-env
-      const branchName = process.env.GITHUB_HEAD_REF || 'main';
-
-      return `https://github.com/${repo}/blob/${branchName}/${filePath}#L${lineNumber}`;
-    } catch (error) {
-      console.log('Could not find .git root, skipping plugin');
-    }
-  }
-};
-
-const getRepository = (gitRoot) => {
-  const result = fs.readFileSync(gitRoot + '/.git/config', 'utf8');
-  const config = ini.parse(result);
-
-  return config['remote "origin"'].url.replace('git@github.com:', '').replace('.git', '');
-};
---
+++ packages/github-url.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const ini = require('ini');
+
+const getGitHubUrl = (localFilePath, lineNumber) => {
+  if (process.env.VERCEL) {
+    // TODO: I bet I'll regret hardcoding this
+    const gitRoot = '/vercel/path0/';
+
+    const repo = process.env.VERCEL_GIT_REPO_OWNER + '/' + process.env.VERCEL_GIT_REPO_SLUG;
+    const filePath = localFilePath.replace(gitRoot, '');
+
+    // TODO: replace with ci-env
+    const branchName = process.env.VERCEL_GIT_COMMIT_REF || 'main';
+
+    return `https://github.com/${repo}/blob/${branchName}/${filePath}#L${lineNumber}`;
+  } else {
+    try {
+      const findGitRoot = require('find-git-root');
+      const gitRoot = findGitRoot(localFilePath).replace('.git', '');
+      const repo = getRepository(gitRoot);
+      const filePath = localFilePath.replace(gitRoot, '');
+
+      // TODO: replace with ci-env
+      const branchName = process.env.GITHUB_HEAD_REF || 'main';
+
+      return `https://github.com/${repo}/blob/${branchName}/${filePath}#L${lineNumber}`;
+    } catch (error) {
+      console.log('Could not find .git root, skipping plugin');
+    }
+  }
+};
+
+const getRepository = (gitRoot) => {
+  const result = fs.readFileSync(gitRoot + '/.git/config', 'utf8');
+  const config = ini.parse(result);
+
+  return config['remote "origin"'].url.replace('git@github.com:', '').replace('.git', '');
+};
+
+module.export = getGitHubUrl;
